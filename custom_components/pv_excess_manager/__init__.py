3ab6cb8c944ba2da129b0f2d110037ddeea83005
Julian Wachholz <julian@wachholz.ch>
2026-03-12 10:19:37 +0100

    Rename integration

--- custom_components/pv_excess_manager/__init__.py
+++ custom_components/pv_excess_manager/__init__.py
@@ -1,8 +1,8 @@
 """
-Custom integration to integrate integration_blueprint with Home Assistant.
+PV Excess Manager integration for Home Assistant.
 
 For more details about this integration, please refer to
-https://github.com/ludeeus/integration_blueprint
+https://github.com/julianwachholz/pv-excess-manager
 """
 
 from __future__ import annotations
--- custom_components/pv_excess_manager/const.py
+++ custom_components/pv_excess_manager/const.py
@@ -1,8 +1,8 @@
-"""Constants for integration_blueprint."""
+"""Constants for pv_excess_manager."""
 
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-DOMAIN = "integration_blueprint"
+DOMAIN = "pv_excess_manager"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
